e5d7c4e3a6e92c2cad2b1486a22c4d0088a32148
saugatnp <saugatnp11@gmail.com>
2021-12-04 13:51:39 +0545

    updated

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +12,7 @@ export class HeaderComponent implements OnInit {
   title: string = 'Todo Tracker';
   showAddTask !: boolean;
   subscription !: Subscription;
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService , private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
   }
 
@@ -20,5 +21,7 @@ export class HeaderComponent implements OnInit {
   toggleAddTask() {
     this.uiService.toggleAddTask();
   }
-
+  hasRoute(Route : string){
+    return this.router.url === Route;
+  }
 }
